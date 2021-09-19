ecc045bb45e93b54a533483ff2402e816878affa
vl4deee11 <boi4enkovlad@yandex>
2021-09-19 18:20:10 +0100

    added z index

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vl4deee11/zidx
+
+go 1.15
---
+++ zidx.go
@@ -0,0 +1,69 @@
+package zidx
+
+func ZIndex(str, subStr string) int {
+	n := len(str)
+	m := len(subStr)
+	if m == 0 {
+		return 0
+	}
+
+	if n == 0 || m > n {
+		return -1
+	}
+
+	lenZ := n + m
+	z := zFunc(subStr, lenZ)
+	li := 0
+	ri := 0
+
+	for i := m; i < lenZ; i++ {
+		if i <= ri {
+			x := ri - i
+			if z[i-li] < x {
+				z[i] = z[i-li]
+			} else {
+				z[i] = x
+			}
+		}
+
+		k := i - m
+		for k+z[i] < n && subStr[z[i]] == str[k+z[i]] {
+			z[i]++
+			if z[i] == m {
+				return k
+			}
+		}
+
+		if i+z[i] > ri {
+			li = i
+			ri = i + z[i]
+		}
+	}
+
+	return -1
+}
+
+func zFunc(s string, lenZ int) []int {
+	li := 0
+	ri := 0
+	z := make([]int, lenZ)
+	for i := 1; i < len(s); i++ {
+		if i <= ri {
+			x := ri - i
+			if z[i-li] < x {
+				z[i] = z[i-li]
+			} else {
+				z[i] = x
+			}
+		}
+		for i+z[i] < len(s) && s[z[i]] == s[i+z[i]] {
+			z[i]++
+		}
+
+		if i+z[i] > ri {
+			li = i
+			ri = i + z[i]
+		}
+	}
+	return z
+}
